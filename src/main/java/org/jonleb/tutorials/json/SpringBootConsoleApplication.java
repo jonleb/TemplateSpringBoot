27227cb4a86c9d0ffaeb9dfebebc2b79297690ef
Jonathan Lebrun <jonathan.lebrun@outlook.be>
2017-11-23 09:25:27 +0100

    Template project

---
+++ src/main/java/org/jonleb/tutorials/json/SpringBootConsoleApplication.java
@@ -0,0 +1,22 @@
+package org.jonleb.tutorials.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jonathan on 30/10/17.
+ */
+@SpringBootApplication
+public class SpringBootConsoleApplication {
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+
+    }
+
+    public void decodeJson(){
+        ObjectMapper mapper = new ObjectMapper();
+
+    }
+}
